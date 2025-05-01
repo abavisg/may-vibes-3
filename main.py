99b5602f58a49be4c1a0448a078158afa08f44d5
Giorgos Ampavis <abavisg@gmail.com>
2025-05-01 12:16:51 +0100

    Added email clinet for imap connectivity

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from dotenv import load_dotenv
 import os
+from email_client import connect_imap
 
 # Load environment variables from .env file
 load_dotenv()
@@ -9,5 +10,6 @@ gmail_address = os.getenv('GMAIL_ADDRESS', 'Not set')
 
 st.title('Smart Inbox Cleaner')
 st.write(f'Connected Gmail address: {gmail_address}')
+st.write(f'IMAP Connection Status: {connect_imap()}')
 
 st.info('Environment and Streamlit are set up! Ready to build.') 
