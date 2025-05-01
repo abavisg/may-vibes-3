840d540fa9452c0540c95c75493b4f9bd02b4dbb
Giorgos Ampavis <abavisg@gmail.com>
2025-05-01 12:16:51 +0100

    Added email clinet for imap connectivity

---
+++ email_client.py
@@ -0,0 +1,17 @@
+from imapclient import IMAPClient
+import os
+
+def connect_imap():
+    imap_host = os.getenv('EMAIL_HOST', 'imap.gmail.com')
+    imap_user = os.getenv('EMAIL_USERNAME', os.getenv('GMAIL_ADDRESS', 'Not set'))
+    imap_pass = os.getenv('EMAIL_PASSWORD', None)
+
+    if imap_user and imap_pass:
+        try:
+            with IMAPClient(imap_host) as server:
+                server.login(imap_user, imap_pass)
+                return f'Connected to {imap_host} as {imap_user}'
+        except Exception as e:
+            return f'Connection failed: {e}'
+    else:
+        return 'IMAP credentials not set in .env file.' 
